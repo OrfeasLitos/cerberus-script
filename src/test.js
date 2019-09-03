01113934249435a916fb95a6f0e8ecd7fa8c4415
Orfeas Stefanos Thyfronitis Litos <o.thyfronitis@ed.ac.uk>
2019-09-03 18:41:06 +0100

    Check if commitment tx is good with dummy input

--- src/test.js
+++ src/test.js
@@ -1,12 +1,23 @@
 const bcoin = require('bcoin')
+const sha256 = require('bcrypto/lib/sha256')
 const KeyRing = bcoin.primitives.KeyRing
 
-const ComTX = require('./txs')
+const WchTwr = require('./watchtower')
 
 const rev_key1 = KeyRing.generate().publicKey
 const rev_key2 = KeyRing.generate().publicKey
 const delay = 42
 const del_key = KeyRing.generate().publicKey
+const aliceCoins = 10
+const bobCoins = 20
+const hash = sha256.digest(Buffer.from('')).toString('hex')
 
-const comTX = new ComTX({rev_key1, rev_key2, delay, del_key, prevout : {hash: '0', index: 0}})
+const ctx = WchTwr.commitmentTX({
+  prevout: {hash, index: 0},
+  aliceOut: {aliceColKey: rev_key1, wRevKey1: rev_key2,
+             aliceDelKey: del_key, bobDelay: delay, aliceCoins},
+  bobOut: {bobColKey: rev_key1, wRevKey2: rev_key2,
+           bobDelKey: del_key, aliceDelay: delay, bobCoins}
+})
 
+console.log(ctx)
