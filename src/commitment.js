879fac70411f970d1ecd05d0b9ef1c7ee80f03db
Orfeas Stefanos Thyfronitis Litos <o.thyfronitis@ed.ac.uk>
2019-09-07 15:07:33 +0100

    Refactor inputs to commitmentTX functions

--- src/commitment.js
+++ src/commitment.js
@@ -23,11 +23,11 @@ function verifyInput(rings, delays, coins, wRevRing1, wRevRing2) {
   ) // TODO: discuss if equality desired
 }
 
-function getFundingCoin(prevout, ring1, ring2, coins1, coins2, fee) {
-  ring1.script = Script.fromMultisig(2, 2,
-    Utils.orderKeys(ring1.publicKey, ring2.publicKey)
+function getFundingCoin(prevout, ring, pubKey1, pubKey2, coins1, coins2, fee) {
+  ring.script = Script.fromMultisig(2, 2,
+    Utils.orderKeys(pubKey1, pubKey2)
   )
-  const address = ring1.getAddress()
+  const address = ring.getAddress()
   const script = Script.fromAddress(address)
   const txinfo = {
     hash: prevout.hash,
@@ -38,20 +38,20 @@ function getFundingCoin(prevout, ring1, ring2, coins1, coins2, fee) {
   return Coin.fromOptions(txinfo)
 }
 
-function addCommitmentOutput(tx, colRing, watchRing, delay, delRing, coins) {
-  const [key1, key2] = Utils.orderKeys(colRing.publicKey, watchRing.publicKey)
-  const witnessScript = Scripts.commScript(key1, key2, delay, delRing.publicKey)
+function addCommitmentOutput(tx, colKey, watchKey, delay, delKey, coins) {
+  const [key1, key2] = Utils.orderKeys(colKey, watchKey)
+  const witnessScript = Scripts.commScript(key1, key2, delay, delKey)
   const outputScript = Utils.outputScrFromWitScr(witnessScript)
 
   tx.addOutput(outputScript, coins)
 }
 
-async function addCommitmentInput(ctx, prevout, fundRing1, fundRing2, coins1, coins2, fee) {
-  const fundingCoin = getFundingCoin(prevout, fundRing1, fundRing2, coins1, coins2, fee)
-  const changeAddress = fundRing1.getAddress()
+async function addCommitmentInput(ctx, prevout, ring, pubKey1, pubKey2, coins1, coins2, fee) {
+  const fundingCoin = getFundingCoin(prevout, ring, pubKey1, pubKey2, coins1, coins2, fee)
+  const changeAddress = ring.getAddress()
 
   await ctx.fund([fundingCoin], {changeAddress})
-  ctx.scriptInput(0, fundingCoin, fundRing1)
+  ctx.scriptInput(0, fundingCoin, ring)
 }
 
 async function getCommitmentTX({
@@ -69,10 +69,19 @@ async function getCommitmentTX({
 
   const ctx = new MTX()
 
-  addCommitmentOutput(ctx, aliceColRing, wRevRing1, bobDelay, aliceDelRing, aliceCoins)
-  addCommitmentOutput(ctx, bobColRing, wRevRing2, aliceDelay, bobDelRing, bobCoins)
+  addCommitmentOutput(
+    ctx, aliceColRing.publicKey, wRevRing1.publicKey, bobDelay,
+    aliceDelRing.publicKey, aliceCoins
+  )
+
+  addCommitmentOutput(
+    ctx, bobColRing.publicKey, wRevRing2.publicKey, aliceDelay,
+    bobDelRing.publicKey, bobCoins
+  )
+
   await addCommitmentInput(
-    ctx, prevout, aliceFundRing, bobFundRing, aliceCoins, bobCoins, fee
+    ctx, prevout, aliceFundRing, aliceFundRing.publicKey,
+    bobFundRing.publicKey, aliceCoins, bobCoins, fee
   )
 
   return ctx
